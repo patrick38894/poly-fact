b52087c37f63d2af0de91116b02b3f980467379c
patrick <snixromero@gmail.com>
2015-02-26 23:52:55 -0800

    syntax

--- util.hs
+++ util.hs
@@ -1,46 +1,47 @@
 import Data.Matrix
+import Data.Maybe
 
 
-interpolate :: [(Num, Num)] -> Matrix Num
+interpolate :: (Fractional a, Ord a) => [(a,a)] -> Matrix a
 
 interpolate xs = let n = length xs
 		     m = matrix n n $ \(i,j) -> (fst (xs !! (i-1))) ^ (n-j)
 		     p = gaussJordElim m $ matrix n 1 $ \(i,j) -> snd (xs !! (i-1))
-		 in toList p
+		 in matToList p
 
 
-solveLower :: Matrix Num -> Matrix Num -> [Num]
+solveLower :: (Fractional a, Ord a) => Matrix a -> Matrix a -> [a]
 
 solveLower l b
 	| nrows b == 0 = []
-	| otherwise =  (y / x):solveLower m` b`
+	| otherwise =  (y / x):solveLower m' b'
 	where x = l ! (1,1)
 	      y = b ! (1,1)
-	      m` = minorMatrix 1 1 m
-	      b` = matrix (nrows b -1) 1 $ \(i,j) -> (b ! (i+1,j))*(1-y/x)
+	      m' = minorMatrix 1 1 l
+	      b' = matrix (nrows b -1) 1 $ \(i,j) -> (b ! (i+1,j))*(1-y/x)
 
-solveUpper :: Matrix Num -> Matrix Num -> [Num]
+solveUpper :: (Fractional a, Ord a) => Matrix a -> Matrix a -> [a]
 
 
 solveUpper u y 
 	| nrows y == 0 = []
-	| otherwise = (b / a):f u' y'
+	| otherwise = (b / a):solveUpper u' y'
 	where a = u ! (nrows u,ncols u)
-	      b = y ! (1,nrows b)
+	      b = y ! (1,nrows y)
 	      u' = minorMatrix (nrows u) (ncols u) u
 	      y' = matrix (nrows y -1) 1 $ \(i,j) -> (y ! (i+1,j))*(1-b/a)
 
 
-gaussJordElim :: Matrix Num -> Matrix Num -> Matrix Num
+gaussJordElim :: (Fractional a, Ord a) => Matrix a -> Matrix a -> Matrix a
 
-gaussJordElim a b = let (l,u,p,d) = luDecomp a
+gaussJordElim a b = let (l,u,p,d) = fromJust (luDecomp a)
 			y = solveLower l (p * b)
 			x = solveUpper u $ fromList (length y) 1 y
 		    in fromList (length x) 1 x
 
-toList :: Matrix a -> [a]
+matToList :: Matrix a -> [a]
 
-toList x = if nrows x == 0
+matToList x = if nrows x == 0
 	   then []
-	   else (x ! (1,1)):minorMatrix 1 0 x
+	   else (x ! (1,1)):(matToList $ minorMatrix 1 0 x)
 
