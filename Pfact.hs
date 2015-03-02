b6a72a12ab8e5ed1e0c000d4b74a0c336a4b43f0
patrick <snixromero@gmail.com>
2015-03-02 14:23:00 -0800

    located bug regarding non-integer division. started implementing fixes via maybe monads. still some typing errors

--- Pfact.hs
+++ Pfact.hs
@@ -1,4 +1,5 @@
 import Util
+import Data.Ratio
 import Data.Maybe
 
 pfactor :: [Int] -> [[Int]]
@@ -10,7 +11,7 @@ pfactor p = let recPfactor pp m
 					  f = map factorSet s'
 					  np = ndSubsets f
 					  pts = map (zip s) np
-					  q = maybeTake (((=<<) (\y -> if (y == [1]) || (y == [-1]) then Nothing else Just y)) . (\x -> pdiv pp $ intInterpolate x)) pts
+					  q = maybeTake (((=<<) (\y -> if (y == [1]) || (y == [-1]) then Nothing else Just y)) . (\x -> maybeInterpolate x >>= pdiv pp)) pts
 				      in case q of Nothing -> recPfactor pp (m-1)
 						   Just qq -> (pfactor qq) ++ (pfactor $ fromJust $ pdiv pp qq)
 	    in recPfactor p $ (length p `quot` 2) + 1
@@ -24,11 +25,11 @@ remove x (y:ys) = if x == y
 		  else y:(remove x ys)
 
 
-ndSubsets :: [[a]] -> [[a]]
+ndSubsets :: [[a]] -> [[a]] --So NP
 
 ndSubsets [] = [[]]
 ndSubsets (a:as) = case a of [x] -> map ([x] ++) $ ndSubsets as
-                             othewise -> (map ([head a] ++) $ ndSubsets as) ++ (ndSubsets $ (tail a):as)
+                             otherwise -> (map ([head a] ++) $ ndSubsets as) ++ (ndSubsets $ (tail a):as)
 
 
 
@@ -40,9 +41,19 @@ maybeTake f [] = Nothing
 maybeTake f (x:xs) = case f x of Nothing -> maybeTake f xs
 				 Just y -> Just y
 
-intInterpolate :: [(Int, Int)] -> [Int]
+maybeList :: [Maybe a] -> Maybe [a]
 
-intInterpolate l = map ceiling $ interpolate $ map (\(x,y) -> (toRational x, toRational y)) l
+maybeList [] = Just []
+
+maybeList (a:as) = case a of Nothing -> Nothing
+			     Just b -> maybeList (as) >>= (\x -> Just (concat b x))
+
+maybeInterpolate :: [(Int, Int)] -> Maybe [Int]
+
+maybeInterpolate l = let f a = if denominator a == 1
+			     then Just numerator a
+			     else Nothing
+		   in maybeList $ f $ interpolate $ map (\(x,y) -> (toRational x, toRational y)) l
 
 
 
